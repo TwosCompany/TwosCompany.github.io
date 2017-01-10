d305d639394f324c6afeb16340ee02f519743f33
David Shorter <david.shorter@myfoodbag.co.nz>
2017-01-10 22:08:37 +1300

    update hugo install.

--- build/install-hugo.sh
+++ build/install-hugo.sh
@@ -4,8 +4,8 @@ set -x
 set -e
 
 # Install Hugo if not already cached or upgrade an old version.
-if [ ! -e $CIRCLE_BUILD_DIR/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
-  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-  tar xvzf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-  cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 $CIRCLE_BUILD_DIR/bin/hugo
+if [ ! -e .hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
+  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -P .hugo/
+  tar xvzf .hugo/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -C .hugo/
+  cp .hugo/hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 ./hugo
 fi
