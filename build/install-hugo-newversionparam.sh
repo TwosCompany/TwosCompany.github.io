3debd49a2da29c95baa4ff4f5f7b86d550660d9e
David Shorter <shorter.david@gmail.com>
2020-08-30 14:54:55 +1200

    Add param version

---
+++ build/install-hugo-newversionparam.sh
@@ -0,0 +1,15 @@
+
+HUGO_VERSION=0.19
+DL_SUFFIX=Linux-64bit
+
+set -x
+set -e
+
+if [ ! -x local/bin/hugo ]
+then
+  mkdir -p local/bin local/tmp
+  wget -P local/tmp/ https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_${DL_SUFFIX}.tar.gz
+  tar -xvf local/tmp/hugo_${HUGO_VERSION}_${DL_SUFFIX}.tar.gz -C local/tmp
+  mv local/tmp/hugo local/bin/hugo
+  chmod +x local/bin/hugo
+fi
